c297ccf32c294ff7cd351a5392b933d6ffefcefb
Василенко Михаил Александрович <Vasilenko.M.Ale@sberbank.ru>
2019-02-13 14:18:58 +0300

    Androin-json exclusion from spring-boot-starter-test to prevent ClassDefNotFoundError for JSONException

--- common/src/main/java/org/ngsandbox/common/speech/entities/ResponseWrapper.java
+++ common/src/main/java/org/ngsandbox/common/speech/entities/ResponseWrapper.java
@@ -1,13 +1,28 @@
 package org.ngsandbox.common.speech.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"response", "status"})
 public class ResponseWrapper<R> {
     private R response;
     private String msg;
     private ResponseStatus status;
+
+    private ResponseWrapper(R response, String msg, ResponseStatus status) {
+        this.response = response;
+        this.msg = msg;
+        this.status = status;
+    }
+
+    public static <T> ResponseWrapper<T> fine(T value) {
+        return new ResponseWrapper<>(value, "", ResponseStatus.OK);
+    }
+
+    public static <T> ResponseWrapper<T> fail(@NonNull ResponseStatus status, @NonNull String errorMessage) {
+        return new ResponseWrapper<>(null, errorMessage, status);
+    }
 }
